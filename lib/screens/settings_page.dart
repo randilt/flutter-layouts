11dbf6b34c0fc75fa8bc1e620f7738711f6debea
Randil <randiltharusha72@gmail.com>
2024-12-11 13:22:00 +0530

    remove appbar

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -7,79 +7,79 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Sri Lanka'),
-          backgroundColor: Colors.blue,
-        ),
+        // appBar: AppBar(
+        //   title: const Text('Sri Lanka'),
+        //   backgroundColor: Colors.blue,
+        // ),
         body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(16),
-              child: const Center(
-                child: Text(
-                  "Settings",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'Rubik'),
-                ),
-              ),
+      children: [
+        Container(
+          margin: const EdgeInsets.all(16),
+          child: const Center(
+            child: Text(
+              "Settings",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Rubik'),
             ),
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.all(16),
-              child: const Text(
-                "Account settings",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Rubik',
-                  color: Color(0xFFADADAD),
-                ),
-              ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: const EdgeInsets.all(16),
+          child: const Text(
+            "Account settings",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Rubik',
+              color: Color(0xFFADADAD),
             ),
-            const NavButton(
-              text: 'Edit profile',
+          ),
+        ),
+        const NavButton(
+          text: 'Edit profile',
+        ),
+        const NavButton(
+          text: 'Change password',
+        ),
+        const NavButton(
+          text: 'Change username',
+        ),
+        Container(
+          margin: const EdgeInsets.all(16),
+          child: const Center(
+            child: Divider(
+              color: Color(0xFFCACACA),
+              height: 1,
             ),
-            const NavButton(
-              text: 'Change password',
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: const EdgeInsets.all(16),
+          child: const Text(
+            "More",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Rubik',
+              color: Color(0xFFADADAD),
             ),
-            const NavButton(
-              text: 'Change username',
-            ),
-            Container(
-              margin: const EdgeInsets.all(16),
-              child: const Center(
-                child: Divider(
-                  color: Color(0xFFCACACA),
-                  height: 1,
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.all(16),
-              child: const Text(
-                "More",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Rubik',
-                  color: Color(0xFFADADAD),
-                ),
-              ),
-            ),
-            const NavButton(
-              text: 'Push Notifications',
-            ),
-            const NavButton(
-              text: 'About us',
-            ),
-            const NavButton(
-              text: 'Privacy Policy',
-            ),
-          ],
-        ));
+          ),
+        ),
+        const NavButton(
+          text: 'Push Notifications',
+        ),
+        const NavButton(
+          text: 'About us',
+        ),
+        const NavButton(
+          text: 'Privacy Policy',
+        ),
+      ],
+    ));
   }
 }
